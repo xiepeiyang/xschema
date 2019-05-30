51fc130900339b3130b96564be4c28a9e3851c5a
peiyang xie <code@xiepy.me>
2019-05-30 15:39:22 +0800

    update

--- xschema.lua
+++ xschema.lua
@@ -1,8 +1,8 @@
 local cjson = require 'cjson'
 
-local _OK = ''
-
 local _Schema = {}
+
+_Schema.OK = ''
 
 function _Schema.HttpParse(define, req)
     -- read data from get or post
@@ -30,7 +30,7 @@ function _Schema.Parse(define, obj)
         if type(func) == 'table' then
             -- process table, recursive...
             local value, errmsg = _Schema.Parse(func, obj[field])
-            if errmsg ~= _OK then
+            if errmsg ~= _Schema.OK then
                 return nil, errmsg
             end
             r[field] = value
@@ -39,17 +39,14 @@ function _Schema.Parse(define, obj)
             if obj ~= nil then
                 value = obj[field]
             end
-            local default, errmsg = func(value, field)
-            if errmsg ~= _OK then
+            local value, errmsg = func(value, field)
+            if errmsg ~= _Schema.OK then
                 return nil, errmsg
-            elseif default ~= nil then
-                r[field] = default
-            else
-                r[field] = value
             end
+            r[field] = value
         end
     end
-    return r, _OK
+    return r, _Schema.OK
 end
 
 function _Schema.Validate(define, obj)
@@ -57,59 +54,59 @@ function _Schema.Validate(define, obj)
     return errmsg
 end
 
--- check default and nonil
 function _Schema.__common(opt, obj, field)
-    local ISNIL = true
+    -- check nonil
     if obj == nil and opt.nonil == true then
-        return nil, field .. ' is nil', false
+        return nil, field .. ' is nil'
     end
+    -- check default
     if obj == nil and opt.default ~= nil then
-        return opt.default, _OK, false
+        return opt.default, _Schema.OK
     end
-    if obj == nil then
-        -- nil is allow
-        return nil, _OK, ISNIL
-    end
-    return nil, _OK, false
+    return nil, _Schema.OK
 end
 
 function _Schema.Boolen(opt)
     local function _check(obj, field)
-        local default, errmsg, isnil = _Schema.__common(opt, obj, field)
-        if default ~= nil or errmsg ~= _OK or isnil then
+        local default, errmsg = _Schema.__common(opt, obj, field)
+        if default ~= nil or errmsg ~= _Schema.OK then
             return default, errmsg
         end
-
-        if type(obj) ~= 'boolen' then
-            return  nil, _errmsg(field, obj, 'is not boolen')
+        if obj == nil then
+            return nil, _Schema.OK
         end
-        return nil, _OK
+        if obj ~= 'true' and obj ~= 'True' and tonumber(obj) ~= 1 then
+            return false, _Schema.OK
+        end
+        return true, _Schema.OK
     end
     return _check
 end
 
 function _Schema.String(opt)
     local function _check(obj, field)
-        local default, errmsg, isnil = _Schema.__common(opt, obj, field)
-        if default ~= nil or errmsg ~= _OK or isnil then
+        local default, errmsg = _Schema.__common(opt, obj, field)
+        if default ~= nil or errmsg ~= _Schema.OK then
             return default, errmsg
         end
-
+        if obj == nil then
+            return nil, _Schema.OK
+        end
         if type(obj) ~= 'string' then
             return nil, _errmsg(field, obj, 'is not string')
         end
-        return nil, _OK
+        return obj, _Schema.OK
     end
     return _check
 end
 
 function _Schema.Number(opt)
     local function _check(obj, field)
-        local default, errmsg, isnil = _Schema.__common(opt, obj, field)
-        if default ~= nil or errmsg ~= _OK or isnil then
+        local default, errmsg = _Schema.__common(opt, obj, field)
+        if default ~= nil or errmsg ~= _Schema.OK then
             return default, errmsg
         end
-
+        obj = tonumber(obj)
         if type(obj) ~= 'number' then
             return nil, _errmsg(field, obj, 'is not number')
         end 
@@ -119,7 +116,7 @@ function _Schema.Number(opt)
         if opt.max ~= nil and opt.max < obj then
             return nil, _errmsg(field, obj, 'is larger than', opt.max)
         end
-        return nil, _OK
+        return obj, _Schema.OK
     end
     return _check
 end
--- xschema_example.lua
+++ xschema_example.lua
@@ -1,4 +1,4 @@
-local schema = require 'schema'
+local schema = require 'xschema'
 
 -- step 1: define schema
 Req = {
@@ -7,6 +7,7 @@ Req = {
     filter = {
         name = schema.String({nonil=true}),
         category = schema.String(),
+        alive = schema.Boolen(),
         age_between = {
             tbeg = schema.Number({min=0}),
             tend = schema.Number({min=1, max=100})
@@ -25,11 +26,13 @@ req, errmsg = schema.Parse(Req, req_var)
 print('case 2: ' .. errmsg)
 
 -- case 3
-req_var = {page=2, filter={name='Jack', category='cat', age_between={tbeg=3}}}
+req_var = {page=2, filter={name='Jack', category='cat', alive=2, age_between={tbeg=3, tend=5}}}
 req, errmsg = schema.Parse(Req, req_var)
+print(errmsg)
 print('\ncase 3')
 print('page: ' .. req.page)
 print('pagesize: ' .. req.pagesize)
 print('filter.name: ' .. req.filter.name)
 print('filter.category: ' .. req.filter.category)
+print('filter.alive: ' .. tostring(req.filter.alive))
 print('filter.age_between.tbeg: ' .. req.filter.age_between.tbeg)
